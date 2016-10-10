5a874313563759c3d29633afcb36728414f9198c
vipulkothifoda <https://github.com/vipulkothifoda>
2016-10-10 02:37:11 +0000

    Create-Destroy Setup

--- create-env.sh
+++ create-env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-06b94666 --key-name cloudvipul --security-group-ids sg-edf82d94 --instance-type t2.micro --placement AvailabilityZone=us-west-2a --count 3 --client-token cloudvipul544 --user-data file://installapp.sh 
+aws ec2 run-instances --image-id ami-06b94666 --key-name cloudvipul --security-group-ids sg-edf82d94 --instance-type t2.micro --placement AvailabilityZone=us-west-2b --count 3 --client-token cloudvipulo544 --user-data file://installapp.sh 
 
 cloud=`aws ec2 describe-instances  --query 'Reservations[*].Instances[].InstanceId' --filters "Name=instance-state-name, Values=pending"`
 
 aws ec2 wait instance-running --instance-ids $cloud
 
-aws elb create-load-balancer --load-balancer-name itmo544-cloudvipul --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2a
+aws elb create-load-balancer --load-balancer-name itmo544-cloudvipul --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
 
 #aws ec2 wait instance-running --instance-ids $cloud
 
@@ -14,6 +14,6 @@ aws elb register-instances-with-load-balancer --load-balancer-name itmo544-cloud
 
 aws autoscaling create-launch-configuration --launch-configuration-name cloudserver --image-id ami-06b94666 --key-name cloudvipul --instance-type t2.micro --user-data file://installapp.sh
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name cloudserverdemo --launch-configuration-name cloudserver --availability-zones us-west-2a --load-balancer-name itmo544-cloudvipul --max-size 5 --min-size 0 --desired-capacity 1
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name cloudserverdemo --launch-configuration-name cloudserver --availability-zones us-west-2b --load-balancer-name itmo544-cloudvipul --max-size 5 --min-size 0 --desired-capacity 1
 
 
